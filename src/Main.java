06992d283cecd60f4d1020a139e6adcc936eb46a
Alex <loalantref@hmail.com>
2022-11-22 19:48:13 +1200

    начали

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+        System.out.println("_____________________________________________________________________________");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
+        }
+        System.out.println("_____________________________________________________________________________");
+        for (int i = 2; i < 18; i+=2) {
+            System.out.println(i);
+        }
+        System.out.println("_____________________________________________________________________________");
+
+
+
+
+
+
+
+
     }
 }
