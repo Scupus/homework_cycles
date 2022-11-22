ffd407c313a285350183628779a9b84387122212
Alex <loalantref@hmail.com>
2022-11-22 20:16:18 +1200

    начали

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i < 11; i++) {
@@ -12,6 +14,26 @@ public class Main {
             System.out.println(i);
         }
         System.out.println("_____________________________________________________________________________");
+        for (int i = 10; i > -11; i--) {
+            System.out.println(i);
+        }
+        System.out.println("_____________________________________________________________________________");
+        int salary = 29000;
+        int total =0;
+        for (int i = 0; i < 13; i++) {
+            total = total + salary;
+            System.out.println("Сумма накоплений = " + total);
+        }
+        System.out.println("_____________________________________________________________________________");
+        int salary2 = 29000;
+        int total2 = 0;
+        for (int i = 0; i < 13; i++) {
+            total2 = (int) (total2 + 0.01 * total2);
+            total2 = total2 + salary2;
+            System.out.println("Месяц " + i + "," + " сумма накоплений = " + total2);
+        }
+        System.out.println("_____________________________________________________________________________");
+
 
 
 
